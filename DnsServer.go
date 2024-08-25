71f2c243f2f33c205614627d6fa0de98777ffd0e
SafiAchraf <a_safi@estin.dz>
2024-08-25 17:36:10 +0100

    adding the whole Dns struct

--- DnsServer.go
+++ DnsServer.go
@@ -7,47 +7,17 @@ import (
 	"strings"
 )
 
-type DNSHeader struct {
-    ID      uint16 
-    Flags   uint16 
-    QDCount uint16 
-    ANCount uint16 
-    NSCount uint16 
-    ARCount uint16 
-}
 
-func (h *DNSHeader) ToBytes() ([]byte, error) {
-    buffer := make([]byte, 12)
-    binary.BigEndian.PutUint16(buffer[0:2], h.ID)
-    binary.BigEndian.PutUint16(buffer[2:4], h.Flags)
-    binary.BigEndian.PutUint16(buffer[4:6], h.QDCount)
-    binary.BigEndian.PutUint16(buffer[6:8], h.ANCount)
-    binary.BigEndian.PutUint16(buffer[8:10], h.NSCount)
-    binary.BigEndian.PutUint16(buffer[10:12], h.ARCount)
-    return buffer, nil
-}
 
-func (h *DNSHeader) DnsHeaderFromBytes(data []byte) error {
-    if len(data) < 12 {
-        return fmt.Errorf("data too short to be a valid DNS header")
-    }
-    h.ID = binary.BigEndian.Uint16(data[0:2])
-    h.Flags = binary.BigEndian.Uint16(data[2:4])
-    h.QDCount = binary.BigEndian.Uint16(data[4:6])
-    h.ANCount = binary.BigEndian.Uint16(data[6:8])
-    h.NSCount = binary.BigEndian.Uint16(data[8:10])
-    h.ARCount = binary.BigEndian.Uint16(data[10:12])
-    return nil
-}
 
-type DNSquestion struct {
+type DNSQuestion struct {
     domain string
     QuesType uint16
     QuesClass uint16
 }
 
 
-func (q *DNSquestion) ToBytes() ([]byte , error){
+func (q *DNSQuestion) serialize() ([]byte , error){
     DomainParts := []byte{}
 
     for _,part := range strings.Split(q.domain, ".") {
@@ -66,7 +36,7 @@ func (q *DNSquestion) ToBytes() ([]byte , error){
     return buffer, nil
 }
 
-func (q * DNSquestion) DnsQuestionFromBytes(rawBytes [] byte) error {
+func (q * DNSQuestion) DnsQuestionFromBytes(rawBytes [] byte) error {
     q.domain = ""
     i := 0
 
@@ -100,7 +70,7 @@ func (q * DNSquestion) DnsQuestionFromBytes(rawBytes [] byte) error {
 }
 
 
-type DNSAnswer struct {
+type DNSResourceRecords  struct {
     Name       string 
     Type       uint16 
     Class      uint16 
@@ -109,35 +79,35 @@ type DNSAnswer struct {
     RData      []byte 
 }
 
-func (a *DNSAnswer) ToBytes() ([]byte, error) {
+func (answer *DNSResourceRecords ) serialize() ([]byte, error) {
     nameParts := []byte{}
-    for _, part := range strings.Split(a.Name, ".") {
+    for _, part := range strings.Split(answer.Name, ".") {
         nameParts = append(nameParts, byte(len(part)))
         nameParts = append(nameParts, []byte(part)...)
     }
     nameParts = append(nameParts, 0) 
 
-    buffer := make([]byte, len(nameParts)+10+int(a.DataLength))
+    buffer := make([]byte, len(nameParts)+10+int(answer.DataLength))
     copy(buffer, nameParts)
 
     offset := len(nameParts)
-    binary.BigEndian.PutUint16(buffer[offset:offset+2], a.Type)
+    binary.BigEndian.PutUint16(buffer[offset:offset+2], answer.Type)
     offset += 2
-    binary.BigEndian.PutUint16(buffer[offset:offset+2], a.Class)
+    binary.BigEndian.PutUint16(buffer[offset:offset+2], answer.Class)
     offset += 2
-    binary.BigEndian.PutUint32(buffer[offset:offset+4], a.TTL)
+    binary.BigEndian.PutUint32(buffer[offset:offset+4], answer.TTL)
     offset += 4
-    binary.BigEndian.PutUint16(buffer[offset:offset+2], a.DataLength)
+    binary.BigEndian.PutUint16(buffer[offset:offset+2], answer.DataLength)
     offset += 2
-    copy(buffer[offset:], a.RData)
+    copy(buffer[offset:], answer.RData)
 
     return buffer, nil
 }
 
 
-func (a *DNSAnswer) DnsAnswerFromBytes(data []byte) error {
+func (answer *DNSResourceRecords) FromBytes(data []byte) error {
     i := 0
-    a.Name = ""
+    answer.Name = ""
     
     for i < len(data) && data[i] != 0 {
         labelLength := int(data[i])
@@ -145,10 +115,10 @@ func (a *DNSAnswer) DnsAnswerFromBytes(data []byte) error {
         if i+labelLength > len(data) {
             return fmt.Errorf("invalid DNS answer format: label length exceeds data size")
         }
-        if len(a.Name) > 0 {
-            a.Name += "."
+        if len(answer.Name) > 0 {
+            answer.Name += "."
         }
-        a.Name += string(data[i : i+labelLength])
+        answer.Name += string(data[i : i+labelLength])
         i += labelLength
     }
 
@@ -158,22 +128,90 @@ func (a *DNSAnswer) DnsAnswerFromBytes(data []byte) error {
         return fmt.Errorf("invalid DNS answer format: insufficient data")
     }
 
-    a.Type = binary.BigEndian.Uint16(data[i : i+2])
+    answer.Type = binary.BigEndian.Uint16(data[i : i+2])
     i += 2
-    a.Class = binary.BigEndian.Uint16(data[i : i+2])
+    answer.Class = binary.BigEndian.Uint16(data[i : i+2])
     i += 2
-    a.TTL = binary.BigEndian.Uint32(data[i : i+4])
+    answer.TTL = binary.BigEndian.Uint32(data[i : i+4])
     i += 4
-    a.DataLength = binary.BigEndian.Uint16(data[i : i+2])
+    answer.DataLength = binary.BigEndian.Uint16(data[i : i+2])
     i += 2
 
-    if len(data) < i+int(a.DataLength) {
+    if len(data) < i+int(answer.DataLength) {
         return fmt.Errorf("invalid DNS answer format: insufficient data for RData")
     }
 
-    a.RData = data[i : i+int(a.DataLength)]
+    answer.RData = data[i : i+int(answer.DataLength)]
 
     return nil
+}
+
+type DNSHeader struct {
+    ID      uint16 
+    Flags   uint16 
+    QDCount uint16 
+    ANCount uint16 
+    NSCount uint16 
+    ARCount uint16 
+}
+
+func (h *DNSHeader) serialize() ([]byte, error) {
+    buffer := make([]byte, 12)
+    binary.BigEndian.PutUint16(buffer[0:2], h.ID)
+    binary.BigEndian.PutUint16(buffer[2:4], h.Flags)
+    binary.BigEndian.PutUint16(buffer[4:6], h.QDCount)
+    binary.BigEndian.PutUint16(buffer[6:8], h.ANCount)
+    binary.BigEndian.PutUint16(buffer[8:10], h.NSCount)
+    binary.BigEndian.PutUint16(buffer[10:12], h.ARCount)
+    return buffer, nil
+}
+
+func (h *DNSHeader) DnsHeaderFromBytes(data []byte) error {
+    if len(data) < 12 {
+        return fmt.Errorf("data too short to be a valid DNS header")
+    }
+    h.ID = binary.BigEndian.Uint16(data[0:2])
+    h.Flags = binary.BigEndian.Uint16(data[2:4])
+    h.QDCount = binary.BigEndian.Uint16(data[4:6])
+    h.ANCount = binary.BigEndian.Uint16(data[6:8])
+    h.NSCount = binary.BigEndian.Uint16(data[8:10])
+    h.ARCount = binary.BigEndian.Uint16(data[10:12])
+    return nil
+}
+
+type DNSMessage struct {
+	Header          DNSHeader
+	Questions       []DNSQuestion
+	ResourceRecords []DNSResourceRecords
+}
+
+
+func (message *DNSMessage) serialize() ([]byte , error){
+    buffer := []byte{}
+
+    header , err := message.Header.serialize()
+    if err != nil {
+        return []byte{} , fmt.Errorf("Error in serializing the header")
+    }
+    buffer = append(buffer, header... )
+
+    for _ , question := range message.Questions {
+        questionSerialized , err := question.serialize()
+        if err != nil {
+            return []byte{} , fmt.Errorf("Error in serializing the Question with the Domain : %v " , question.domain) 
+        }
+        buffer = append(buffer, questionSerialized...)
+    }
+
+    for _ , answer := range message.ResourceRecords {
+        RecourceRecords  , err := answer.serialize()
+        if err != nil {
+            return []byte{} , fmt.Errorf("Error in serializing the Answer with the Name : %v " , answer.Name)
+        }
+        buffer = append(buffer, RecourceRecords...)
+    }
+
+    return buffer , nil
 }
 
 
@@ -207,7 +245,7 @@ func main() {
 		}
 		fmt.Println("recieved msg from %s : %s 	\n" , senderAddr , string(buffer[:n]))
 
-		message , err := header.ToBytes()
+		message , err := header.serialize()
 
 		n , err = conn.WriteToUDP(message , senderAddr)
 		if err != nil {
